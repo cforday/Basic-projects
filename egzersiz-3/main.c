f1137585917cfcde79187a709cf441cc8921f328
cforday <cforday@hotmail.com>
2026-01-24 00:50:52 +0300

    egzersiz-3

---
+++ egzersiz-3/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int s1,s2,carp;
+
+    printf("2 sayi giriniz: ");
+    scanf("%d %d",&s1,&s2);
+
+    carp=s1*s2;
+
+    printf("2 Sayinin carpimi: %d",carp);
+    return 0;
+}
