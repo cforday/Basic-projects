cfdc747d94b099eea78953dc1f41587227f26052
cforday <cforday@hotmail.com>
2026-01-24 00:50:45 +0300

    egzersiz-2

---
+++ egzersiz-2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int s1,s2;
+    printf("iki sayi giriniz: ");
+    scanf("%d %d",&s1,&s2);
+
+    printf("1.Sayim: %d\n2.Sayim: %d",s1,s2);
+    return 0;
+}
