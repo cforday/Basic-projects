20dd9bc9a09d9eb34a2a07665586192708ce425a
cforday <cforday@hotmail.com>
2026-01-23 23:10:04 +0300

    C-programlamayi-seviyorum-ekrana-yazdirma

---
+++ egzersiz-1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //C Egzersiz 1
+    //C Programlamayi seviyorum ekrana yazdirma
+
+    printf("C Programlamayi Seviyorum");
+    return 0;
+}
